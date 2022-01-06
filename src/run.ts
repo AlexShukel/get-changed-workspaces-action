acd90a11476a5f6374f9bb75d791eb138b0254b0
Aleksandras Šukelovič <alex.shukel@gmail.com>
2022-01-06 18:35:17 +0200

    Getting changed files

--- src/run.ts
+++ src/run.ts
@@ -1,7 +1,26 @@
 import { getInput } from "@actions/core";
 import { getWorkspaces } from "./getWorkspaces";
+import { exec } from "@actions/exec";
+
+const getChangedFiles = async (ref: string) => {
+    let buffer = "";
+
+    const code = await exec("git", ["diff-index", "--name-only", ref], {
+        listeners: {
+            stdout: (data) => (buffer += data.toString() + " "),
+        },
+    });
+
+    console.log(buffer, code);
+};
 
 export const run = async () => {
     const workspaces = getInput("workspaces") || (await getWorkspaces());
-    console.log(workspaces);
+
+    // get array of paths to changed packages
+    const output: string[] = [];
+    const token = getInput("token");
+
+    console.log(token, workspaces);
+    getChangedFiles("FETCH_HEAD");
 };
