c2c8c2e57fdb4f6266e632bdcfcbfb3dce0a6919
Aleksandras Šukelovič <alex.shukel@gmail.com>
2022-01-06 18:53:41 +0200

    Test git diff

--- src/run.ts
+++ src/run.ts
@@ -3,24 +3,6 @@ import { getWorkspaces } from "./getWorkspaces";
 import { exec } from "@actions/exec";
 import { context } from "@actions/github";
 
-async function fetchCommit(ref: string): Promise<void> {
-    if ((await exec("git", ["fetch", "--depth=1", "--no-tags", "origin", ref])) !== 0) {
-        throw new Error(`Fetching ${ref} failed`);
-    }
-}
-
-const getChangedFiles = async (ref: string) => {
-    let buffer = "";
-
-    const code = await exec("git", ["diff-index", "--name-only", ref], {
-        listeners: {
-            stdout: (data) => (buffer += data.toString() + " "),
-        },
-    });
-
-    console.log(buffer, code);
-};
-
 export const run = async () => {
     const workspaces = getInput("workspaces") || (await getWorkspaces());
 
@@ -28,7 +10,9 @@ export const run = async () => {
     const output: string[] = [];
     const token = getInput("token");
 
-    console.log(token, workspaces);
-    await fetchCommit(context.payload.pull_request?.base.sha);
-    getChangedFiles("FETCH_HEAD");
+    await exec("git", ["diff", "--name-only", context.payload.pull_request?.base.ref], {
+        listeners: {
+            stdout: (data) => console.log(data.toString()),
+        },
+    });
 };
