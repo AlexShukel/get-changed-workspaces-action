fb2e237ddb3e2bbf8a0a35b1e09292a9d47c9bf7
Aleksandras Šukelovič <alex.shukel@gmail.com>
2022-01-07 16:25:16 +0200

    Extracted getWorkspaces function

--- src/run.ts
+++ src/run.ts
@@ -1,29 +1,13 @@
 import { getChangedFiles } from "./getChangedFiles";
-import { getWorkspaces } from "./getWorkspaces";
-import { getWorkspacesFromInput } from "./getWorkspacesFromInput";
-import { getChangedWorkspace } from "./getChangedWorkspace";
 import { setOutput } from "@actions/core";
-import mapWorkspaces from "@npmcli/map-workspaces";
-import fs from "fs";
+import { getWorkspaces } from "./getWorkspaces";
 
 export const run = async () => {
-    // const workspaces = getWorkspacesFromInput() || (await getWorkspaces());
+    const output: string[] = [];
+    const changedFiles = await getChangedFiles();
+    const workspaces = await getWorkspaces();
 
-    // const output: string[] = [];
+    console.log(workspaces);
 
-    // const changedFiles = await getChangedFiles();
-
-    const configSource = await fs.promises.readFile("package.json", { encoding: "utf-8" });
-    console.log(await mapWorkspaces({ pkg: JSON.parse(configSource), cwd: process.cwd() }));
-
-    // changedFiles.forEach((file) => {
-    //     workspaces.forEach((workspace) => {
-    //         const changedWorkspace = getChangedWorkspace(file, workspace);
-    //         if (changedWorkspace) {
-    //             output.push(changedWorkspace);
-    //         }
-    //     });
-    // });
-
-    setOutput("changed_workspaces", []);
+    setOutput("changed_workspaces", output);
 };
