0af07ad3d301a1a95df21638c564f0035129516f
Aleksandras Šukelovič <alex.shukel@gmail.com>
2022-01-08 12:26:47 +0200

    fetching all origin

--- src/getChangedFilesFromGit.ts
+++ src/getChangedFilesFromGit.ts
@@ -3,7 +3,7 @@ import { exec } from "@actions/exec";
 export const getChangedFilesFromGit = async (ref: string): Promise<string[]> => {
     const result: string[] = [];
 
-    await exec("git", ["fetch", "--depth=1", "--no-tags", "origin", ref]);
+    await exec("git", ["fetch", "--depth=1", "--no-tags", "origin", "--all"]);
 
     await exec("git", ["diff", "--name-only", ref], {
         listeners: {
