7872c36042b59bcb7e37e348db74f45837a5bfe5
Aleksandras Šukelovič <alex.shukel@gmail.com>
2022-01-08 11:08:11 +0200

    Logging push payload

--- src/getChangedFiles.ts
+++ src/getChangedFiles.ts
@@ -2,15 +2,22 @@ import { exec } from "@actions/exec";
 import { context } from "@actions/github";
 
 export const getChangedFiles = async (): Promise<string[]> => {
-    const result: string[] = [];
+    if (context.eventName === "pull_request") {
+        const result: string[] = [];
 
-    await exec("git", ["fetch", "--all"]);
+        await exec("git", ["fetch", "--all"]);
 
-    await exec("git", ["diff", "--name-only", `origin/${context.payload.pull_request?.base.ref}`], {
-        listeners: {
-            stdline: (data) => result.push(data),
-        },
-    });
+        await exec("git", ["diff", "--name-only", `origin/${context.payload.pull_request?.base.ref}`], {
+            listeners: {
+                stdline: (data) => result.push(data),
+            },
+        });
 
-    return result;
+        return result;
+    } else if (context.eventName === "push") {
+        console.log(context.payload);
+        return [];
+    } else {
+        throw new Error("This action can be triggered only by pull_request or push event");
+    }
 };
