e735d7feb21164ba12143dd1abfb20b7af8e0870
Aleksandras Sukelovic <alex.shukel@gmail.com>
2022-11-04 14:44:48 +0200

    Parse pnpm-workspace.yaml

--- src/getWorkspaces.ts
+++ src/getWorkspaces.ts
@@ -3,13 +3,35 @@ import path from "path";
 
 import { getInput } from "@actions/core";
 import mapWorkspaces from "@npmcli/map-workspaces";
+import { parse } from "yaml";
 
-export const getWorkspaces = async (): Promise<Map<string, string>> => {
-    const packageDirPath = getInput("working-directory") || process.cwd();
+const getPackageConfig = async (packageDirPath: string) => {
     const packageJsonPath = path.join(packageDirPath, "package.json");
     const configSource = await fs.promises.readFile(packageJsonPath, { encoding: "utf-8" });
     const parsedConfig = JSON.parse(configSource);
+
+    if (!parsedConfig.workspaces) {
+        const pnpmWorkspacesPath = path.join(packageDirPath, "pnpm-workspace.yaml");
+
+        if (fs.existsSync(pnpmWorkspacesPath)) {
+            const file = await fs.promises.readFile(pnpmWorkspacesPath);
+            const pnpmWorkspacesConfig = parse(file.toString());
+
+            console.log("parsed pnpm-workspace.yaml:", pnpmWorkspacesConfig);
+
+            parsedConfig.workspaces = pnpmWorkspacesConfig;
+
+            console.log("Parsed config:", parsedConfig);
+        }
+    }
+
+    return parsedConfig;
+};
+
+export const getWorkspaces = async (): Promise<Map<string, string>> => {
+    const packageDirPath = getInput("working-directory") || process.cwd();
     const workspaces = getInput("workspaces");
+    const parsedConfig = await getPackageConfig(packageDirPath);
 
     if (workspaces) {
         const workspacesArray = workspaces.split("\n").map((val) => val.trim());
